298cda47e214623c4e6f4c48b50a6d2b0e5bbd6b
jackere1 <jackerenh@gmail.com>
2023-01-09 13:28:02 +0800

    added new page and css

--- client.js
+++ client.js
@@ -1,29 +1,43 @@
 const socket = new WebSocket('wss://webgroupchat-server-jacker-bp71.onrender.com', "HTTPS");
 
-const messages = document.getElementById('messages');
+let username = "";
+function enter(e) {
+    console.log("PEZ")
+    e.preventDefault();
+    
+    username = document.getElementById('username').value;
+    document.body.innerHTML = `
+        <link rel="stylesheet" href="style/style.css">
+    `
+    document.body.innerHTML += `
+        <div id="messages"></div>
+        <form onsubmit="msg(event); return false;">
+            <input type="text" placeholder="Aa" maxlength="100" id="text" >
+        </form> 
+    `
+    return false;
+};
+
+function msg(e) {
+    e.preventDefault();
+
+    if (document.getElementById('text').value) {
+        socket.send(document.getElementById('text').value);
+        showMessage(document.getElementById('text').value, true);
+        document.getElementById('text').value = "";
+        document.getElementById('messages').scrollBy(0, 6000);
+    }
+}
+socket.addEventListener('message', e => {
+    e.data.text().then(showMessage)
+    document.getElementById('messages').scrollBy(0, 6000);
+});
 
 const showMessage = (str, isMine = false) => {
-    messages.innerHTML += `
+    document.getElementById('messages').innerHTML += `
         <div class="messageContainer ${isMine ? 'mine' : 'notMine'}">
             <div class="messageCloud">${str}</div>
         </div>
     `
 }
 
-socket.addEventListener('message', e => {
-    e.data.text().then(showMessage)
-    messages.scrollBy(0, 6000);
-});
-
-const input = document.querySelector('input');
-document.querySelector('form').onsubmit = e => {
-    e.preventDefault();
-
-    if (input.value) {   
-        socket.send(input.value);
-        showMessage(input.value, true);
-        input.value = "";
-        messages.scrollBy(0, 6000);
-    }
-
-}
